6a65fefa08e423c75646fea66de6cefe6bfe037b
ArimasPtwi <arimaspertiwi@gmail.com>
2023-10-08 11:47:57 +0800

    first commit in github

---
+++ function.js
@@ -0,0 +1,15 @@
+//FUNGSI TANPA PARAMETER
+function pembuka() {
+    console.log("Halo, Aku Arimas Pertiwi dari Program Web Development👋🏻");
+}
+// Memanggil fungsi
+pembuka();
+
+
+// FUNGSI DENGAN PARAMETER
+function tambahkan(angka1, angka2) {
+  return angka1 + angka2;
+}
+// Memanggil fungsi 
+const hasil = tambahkan(100, 3);
+console.log("Hasil penambahan: " + hasil);
---
+++ switch case.js
@@ -0,0 +1,15 @@
+//SWITCH CASE
+
+const fruit = 'apple';
+
+switch(fruit){
+    case 'apple':
+    console.log('fruit is apple')
+    break;
+    case 'banana':
+    console.log('fruit is kiwi')
+    break;
+    default :
+    console.log('other')
+    break
+}
